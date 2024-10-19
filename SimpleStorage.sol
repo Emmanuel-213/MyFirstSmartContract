b74a68c6b276c75038c9120f3ea7a7ba030ba87e
Abande Abanga Emmanuel <116530341+Emmanuel-213@users.noreply.github.com>
2024-10-19 03:18:04 +0000

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.25; // This allows any version from 0.8.25 to less than 0.9.0
+// SPDX-License-Identifier: MIT
+contract SimpleStrorage{
+    uint256 favoriteNumber; //state a variable to store number
+}
